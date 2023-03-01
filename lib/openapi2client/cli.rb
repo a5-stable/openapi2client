3013c63e08deb95d2be9bc82562b210de482c775
a5-stable <sh07e1916@gmail.com>
2023-03-01 22:06:22 +0900

    update sample

--- lib/openapi2client/cli.rb
+++ lib/openapi2client/cli.rb
@@ -1,4 +1,5 @@
 require 'thor'
+require 'fileutils' 
 
 module Openapi2client
   class CLI < Thor
@@ -6,9 +7,40 @@ module Openapi2client
     desc 'generate FILE_PATH', 'Generate API client code from OpenAPI YAML file'
 
     def generate(file_path)
-      yaml = YAML.load_file(file_path)
-      # OpenAPI YAMLを読み込んでAPIクライアントのコードを生成する処理
-      puts 'Generated API client code'
+      # yaml = YAML.load_file(file_path)
+      # # OpenAPI YAMLを読み込んでAPIクライアントのコードを生成する処理
+      # puts 'Generated API client code'
+
+      data = File.open("sample.json") do |f|
+        JSON.load(f)
+      end
+
+      Dir.mkdir("outputs/")
+      Dir.mkdir("outputs/lib")
+      data["tags"].each do |tag|
+        FileUtils.touch("outputs/lib/#{tag["name"]}.rb")
+      end
+
+      FileUtils.touch("outputs/lib/client.rb")
+
+      template_file_path = "../templates/client.rb"
+      copied_file_path = "outputs/lib/client.rb"
+
+      # Read the content of the template file
+      template_file_content = File.read(template_file_path)
+
+      # Replace the placeholders in the template file content with the passed argument
+      replaced_content = template_file_content.gsub('___API_BASE_URL___', "https://" + data["host"] + data["basePath"])
+
+      # Write the replaced content to the copied file
+      File.write(copied_file_path, replaced_content)
+
+      data["paths"].each do |pathname, hash|
+        method = hash.keys.first
+        target_file = "lib/#{hash[method]["tags"][0]}"
+
+        method_name = operationId
+      end
     end
   end
 end
---
+++ lib/templates/client.rb
@@ -0,0 +1,39 @@
+require 'faraday'
+
+class ApiClient
+  API_BASE_URL = '___API_BASE_URL___'
+
+  def initialize(api_key)
+    @api_key = api_key
+    @connection = Faraday.new(url: API_BASE_URL) do |faraday|
+      faraday.request :url_encoded
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get(resource, params = {})
+    request(:get, resource, params)
+  end
+
+  def post(resource, body = {})
+    request(:post, resource, body)
+  end
+
+  def put(resource, body = {})
+    request(:put, resource, body)
+  end
+
+  def delete(resource)
+    request(:delete, resource)
+  end
+
+  private
+
+  def request(method, resource, data = {})
+    response = @connection.send(method) do |request|
+      request.url(resource, data)
+      request.headers['Authorization'] = "Bearer #{@api_key}"
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
